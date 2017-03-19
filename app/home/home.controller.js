8f9ef1ad40a6367266a639deda85d1645bc05ea1
Ajit Kumar <er.ajitkumar90@gmail.com>
2017-03-19 11:52:00 +0530

    contact us changes

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,7 +1,42 @@
 'use strict';
 
-app.controller('homeCtrl', ['$scope', '$rootScope', '$location', '$q', '$timeout',
-    function($scope, $rootScope, $location, $q, $timeout, $window) {
-    		
+app.controller('homeCtrl', ['$scope', '$rootScope', '$location', '$q', '$timeout', 'toasty', 'query',
+    function($scope, $rootScope, $location, $q, $timeout, toasty, query) {
+
+        $scope.user = {
+            name: "",
+            email: "",
+            contact: "",
+            subject: "",
+            message: ""
+        }
+
+        $scope.sendQuery = function(valid, data) {
+            console.log(valid, "data>>>>>", data)
+            if (valid) {
+            $rootScope.waiting = true;
+                query.send(data).then(function(response) {
+                	 $rootScope.waiting = false;
+                    if (response.authentication === true) {
+                        toasty.success({
+                            title: 'Success! Message',
+                            msg: response.message
+                        });
+                    } else {
+                        toasty.error({
+                            title: 'Error! Message',
+                            msg: response.message
+                        });
+                    }
+
+                })
+            } else {
+                toasty.error({
+                    title: 'Error! Message',
+                    msg: "Please fill all mandatory(*) fields"
+                });
+            }
+        }
+
     }
 ]);
