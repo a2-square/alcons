0cad983f2604b53829fa342acf076d5e934ede02
Ajit Kumar <er.ajitkumar90@gmail.com>
2017-03-28 03:03:03 +0530

    count fix

--- components/footer/footer.controller.js
+++ components/footer/footer.controller.js
@@ -1,18 +1,24 @@
 'use strict';
 
-app.controller('footerCtrl', ['$scope', '$rootScope', '$location', '$q', '$timeout', 'query',
-    function($scope, $rootScope, $location, $q, $timeout, query) {
-        $scope.url = $location.path();
-        $scope.visitor = 0
-        query.visit().then(function(response) {
-            $rootScope.waiting = false;
-            if (response.authentication === true) {
-            	$scope.visitor = response.data.count_alcon.count
-            	console.log("visitor", response)
-            } else {
-            	console.log("visitor errorrrr", response)
-            }
+app.controller('footerCtrl', ['$scope', '$rootScope', '$location', '$q', '$timeout', 'query', '$route',
+    function($scope, $rootScope, $location, $q, $timeout, query, $route) {
+        $scope.url = $location.absUrl();
+        $scope.visitor = (localStorage.getItem('visitor')) ? localStorage.getItem('visitor') : 'loading..';
+        var currentPathIndex = $scope.url.indexOf("localhost");
+        if (currentPathIndex == "-1") {
+        	console.log("getting visitor.....")
+            query.visit().then(function(response) {
+                $rootScope.waiting = false;
+                if (response.authentication === true) {
+                    localStorage.setItem('visitor', response.data.count_alcon.count);
+                    $scope.visitor = localStorage.getItem('visitor');
+                } else {
+                    localStorage.setItem('visitor', 'loading..');
+                    $scope.visitor = localStorage.getItem('visitor');
+                }
 
-        })
+            })
+        }
+
     }
 ]);
